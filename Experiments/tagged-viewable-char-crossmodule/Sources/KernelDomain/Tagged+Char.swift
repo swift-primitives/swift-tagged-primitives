2baf8aad7a0e7e1883f32b5d7e0500246715ca41
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-11 14:17:09 +0200

    Centralize research and experiments corpora

--- Experiments/tagged-viewable-char-crossmodule/Sources/KernelDomain/Tagged+Char.swift
+++
@@ -1,3 +0,0 @@
-// KernelDomain module: no ad-hoc Char extensions needed
-// Char is provided by StringDomain (Tagged where RawValue == MyString)
-// and PathDomain (Tagged where RawValue == MyPath)
